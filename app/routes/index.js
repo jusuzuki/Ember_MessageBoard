60c94438f57bef855611da8cb0841c7e74da62bf
jusuzuki <jsayumi@gmail.com>
2015-10-02 22:12:15 -0700

    Add Firebase database

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,21 +1,7 @@
 import Ember from 'ember';
 
-var questions = [{
-  user: "Regina Mills",
-  subject: "Why is guyliner-handless-wonder still around again?",
-  notes: "Evil Regals!"
-}, {
-  user: "Emma Swan",
-  subject: "EEE",
-  notes: "SSS"
-}, {
-  user: "Root & Shaw",
-  subject: "RRR",
-  notes: "SSS"
-}];
-
 export default Ember.Route.extend({
   model() {
-    return questions;
+    return this.store.findAll('question');
   },
 });
