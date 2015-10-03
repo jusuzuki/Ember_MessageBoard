fa160ac9651a9dea4190f6e0c9f4b0a11c8e7a2a
jusuzuki <jsayumi@gmail.com>
2015-10-03 07:30:59 -0700

    Add update-question functionality

--- app/routes/question.js
+++ app/routes/question.js
@@ -6,9 +6,20 @@ export default Ember.Route.extend({
   },
 
   actions: {
-  destroyQuestion(question) {
-    question.destroyRecord();
-    this.transitionTo('index');
-    }
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    },
+
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+
   }
 });
