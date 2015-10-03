338468bc57692195990339f5317c239a76721684
jusuzuki <jsayumi@gmail.com>
2015-10-02 22:21:56 -0700

    Function to delete questions

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,14 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+
+  actions: {
+
+    delete(question) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        question.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
+  }
 });
