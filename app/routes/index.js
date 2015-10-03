d6b97e780444f3a9a572313a15f4833caaee01af
jusuzuki <jsayumi@gmail.com>
2015-10-02 23:54:27 -0700

    Add individual pages per question, updated delete function

--- app/routes/index.js
+++ app/routes/index.js
@@ -10,13 +10,7 @@ export default Ember.Route.extend({
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
-    },
-
-    delete(question) {
-      if (confirm('Are you sure you want to delete this rental?')) {
-        question.destroyRecord();
-        this.transitionTo('index');
-      }
     }
   }
+
 });
